cdd1bb44582c484133b2924af54a0ca3604c7fbf
ayato <ayato1925@gmail.com>
2022-03-31 18:52:47 +0900

    MemoApp design Create

--- App.jsx
+++ App.jsx
@@ -4,18 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>Logout</Text>
+      <View style={styles.AppBar}>
+        <View style={styles.AppBarInner}>
+          <Text style={styles.AppBarTitle}>Memo App</Text>
+          <Text style={styles.AppBarRight}>Logout</Text>
         </View>
       </View>
 
       <View>
-        <View>
+        <View style={styles.MemoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2022年03月30日</Text>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -24,10 +24,10 @@ export default function App() {
       </View>
 
       <View>
-        <View>
+        <View style={styles.MemoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2022年03月30日</Text>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -36,10 +36,10 @@ export default function App() {
       </View>
 
       <View>
-        <View>
+        <View style={styles.MemoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2022年03月30日</Text>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -48,7 +48,115 @@ export default function App() {
       </View>
 
       <View>
-        <Text>+</Text>
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.MemoListItem}>
+          <View>
+            <Text style={styles.MemoListTitle}>買い物リスト</Text>
+            <Text style={styles.MemoListDate}>2022年03月30日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.circleButton}>
+        <Text style={styles.circleLabel}>+</Text>
       </View>
     </View>
   );
@@ -56,9 +164,71 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, // flexbox表示範囲
+    backgroundColor: '#F0F4F8', // 背景色
+  },
+  AppBar: {
+    width: '100%', // 幅
+    height: 104, // 高さ
+    backgroundColor: '#467FD3',
+    justifyContent: 'flex-end', // flexboxに対する縦間隔をどう取るか
+  },
+  AppBarInner: {
+    alignItems: 'center', // flexboxに対する横間隔をどう取るか
+  },
+  AppBarRight: {
+    position: 'absolute', // 位置
+    right: 19,
+    bottom: 16,
+    color: 'rgba( 255, 255, 255, 0.8 )', // 透明度を持たせた色指定
+  },
+  AppBarTitle: {
+    margin: 8, // 外方向の間隔
+    fontSize: 22, // 文字のサイズ
+    lineHeight: 32, // 行の高さ
+    color: '#ffffff', // 文字色
+    fontWeight: 'bold', // 文字の太さ
+  },
+  MemoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row', // 主軸の方向や向き
+    justifyContent: 'space-between',
+    paddingVertical: 16, // 内方向の間隔
+    paddingHorizontal: 19,
     alignItems: 'center',
+    borderBottomWidth: 1, // 枠線
+    borderColor: 'rgba( 0, 0, 0, 0.15 )', // 枠線色
+  },
+  MemoListTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  MemoListDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32, // 枠線に丸みをつける
     justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    // IOS shadow
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    // android shadow
+    elevation: 5,
+  },
+  circleLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
